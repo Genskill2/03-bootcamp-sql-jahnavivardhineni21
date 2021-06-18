20662e43fbe43f6684aa7d2f09a730d2529ffeea
jahnavivardhineni21 <84776062+jahnavivardhineni21@users.noreply.github.com>
2021-06-18 16:35:53 +0530

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -1,6 +1,6 @@
-INSERT INTO publisher VALUES(1,"PHI", "India");
-INSERT INTO publisher VALUES(2,"Harper", "USA");
-INSERT INTO publisher VALUES(3,"GCP", "USA");
+INSERT INTO publisher VALUES(1,"PHI","India");
+INSERT INTO publisher VALUES(2,"Harper","USA");
+INSERT INTO publisher VALUES(3,"GCP","USA");
 INSERT INTO publisher VALUES(4,"Avery","USA");
 INSERT INTO publisher VALUES(5,"Del Rey","UK");
 INSERT INTO publisher VALUES(6,"Vintage","UK");
